ae9a1c7bee7263dab855f5e1748e7f9768101c1d
Natalia Davydova <davydovanatalia063@gmail.com>
2021-05-30 14:05:02 -0700

    add html markup, styles, index.js, event change for checkbox

---
+++ src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let checkbox = document.getElementById('discount-checkbox');
+checkbox.addEventListener('change', function () {
+  if (this.checked === true) {
+    this.nextElementSibling.classList.add('checked');
+  } else {
+    this.nextElementSibling.classList.remove('checked');
+  }
+})
