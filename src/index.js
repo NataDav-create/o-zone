a25fbf52e3c1cbf23a582160afba141b6f80d598
Natalia Davydova <davydovanatalia063@gmail.com>
2021-05-30 21:00:31 -0700

    add open and close cart functions, function to display items in cart

--- src/index.js
+++ src/index.js
@@ -8,3 +8,37 @@ checkbox.addEventListener('change', function () {
     this.nextElementSibling.classList.remove('checked');
   }
 })
+
+const btnCart = document.getElementById('cart');
+const modalCart = document.querySelector('.cart');
+const closeBtn = document.querySelector('.cart-close');
+
+btnCart.addEventListener('click', () => {
+  modalCart.style.display = 'flex';
+  document.body.style.overflow = 'hidden';
+});
+
+closeBtn.addEventListener('click', () => {
+  modalCart.style.display = 'none';
+  document.body.style.overflow = '';
+});
+
+const cards = document.querySelectorAll('.goods .card');
+const cartWrapper = document.querySelector('.cart-wrapper');
+const cartEmpty = document.getElementById('cart-empty');
+const countGoods = document.querySelector('.counter');
+cards.forEach((card) => {
+  const btn = card.querySelector('button');
+  btn.addEventListener('click', () => {
+    const cardClone = card.cloneNode(true);
+    cartWrapper.appendChild(cardClone);
+    cartEmpty.remove();
+    showData();
+  })
+});
+
+function showData() {
+  const cardsCart = cartWrapper.querySelectorAll('.card');
+  countGoods.textContent = cardsCart.length;
+
+}
