715739d96fd6d5befb13211a2473cedb6c97ee11
Natalia Davydova <davydovanatalia063@gmail.com>
2021-05-31 19:52:58 -0700

    add filter by min and max

--- src/index.js
+++ src/index.js
@@ -76,6 +76,11 @@ function addCart() {
 function actionPage() {
   const cards = document.querySelectorAll('.goods .card');
   const discountCheckbox = document.getElementById('discount-checkbox');
+  const min = document.getElementById('min');
+  const max = document.getElementById('max');
+  const search = document.querySelector('.search-wrapper_input');
+  const searchBtn = document.querySelector('.search-btn');
+
   discountCheckbox.addEventListener('click', () => {
     cards.forEach((card) => {
       if (discountCheckbox.checked) {
@@ -85,8 +90,22 @@ function actionPage() {
       } else {
         card.parentNode.style.display = '';
       }
+    });
+  });
+
+  function filterPrice() {
+    cards.forEach((card) => {
+      const cardPrice = card.querySelector('.card-price');
+      const price = parseFloat(cardPrice.textContent);
+      if ((min.value && price < min.value) || (max.value && price > max.value)) {
+        card.parentNode.style.display = 'none';
+      } else {
+        card.parentNode.style.display = '';
+      }
     })
-  })
+  };
+  min.addEventListener('change', filterPrice)
+  max.addEventListener('change', filterPrice)
 }
 
 toggleCheckbox();
